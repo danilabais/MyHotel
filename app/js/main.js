86a85e27d3144619a44bcc3370d70cff0274ce7c
Danila <da_buddha@bk.ru>
2021-06-30 00:54:39 +0300

    new project

---
+++ app/js/main.js
@@ -0,0 +1,58 @@
+const swiper = new Swiper('.main-fullpage', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    
+  });
+
+
+
+
+const screenWidth = window.screen.width
+  window.addEventListener('scroll', function() {
+      
+      let last_known_scroll_position = window.pageYOffset;
+      if (screenWidth>1100) {
+        if (last_known_scroll_position<=100)  {
+          document.querySelector('.header').classList.remove('scrolling');
+        }
+        else document.querySelector('.header').classList.add('scrolling');
+
+      }
+      });
+
+  document.getElementsByClassName("burger")[0].addEventListener("click", e => {
+    document.getElementsByClassName("header")[0].classList.toggle('active')
+  
+});
+
+
+
+var map = L.map('map', {
+  scrollWheelZoom:false,
+}).setView([60.025502, 30.341760], 16);
+
+var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="copyright">Openstreetmap</a>'
+}).addTo(map);
+
+
+var baseMaps = {
+  OSM: osmLayer,
+  Spinal: spinalLayer,
+};
+
+var marker2 = L.marker([60.025502, 30.341760], {
+  
+}).addTo(map);
+marker2.addTo(map);
+
+
+
+
